d041a4d3b1ccce31c87fef85cd2361c50276fc04
Braga <ibrahim.houmaidi@gmail.com>
2025-07-11 12:22:50 +0200

    Forms: Implement authentication and content management forms

--- holbydoc/forms.py
+++ holbydoc/forms.py
@@ -1,7 +1,12 @@
 from tokenize import String
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from flask_ckeditor import CKEditorField
+from flask_login import current_user
+from flask_wtf.file import FileAllowed, FileField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, ValidationError 
+from holbydoc.models import User, Course
 
 
 class RegistrationForm(FlaskForm):
@@ -18,7 +23,7 @@ class RegistrationForm(FlaskForm):
         validators=[
             DataRequired(),
             Regexp(
-                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,32}$"
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&_])[A-Za-z\\d@$!%*?&_]{8,32}$"
             ),
         ],
     )
@@ -26,6 +31,18 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "Username already exists! Please chosse a different one"
+            )
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already exists! Please chosse a different one")
 
 
 class LoginForm(FlaskForm):
@@ -38,3 +55,90 @@ class LoginForm(FlaskForm):
     )
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log In")
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=25)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    bio = TextAreaField("Bio")
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
+    )
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    "Username already exists! Please chosse a different one"
+                )
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    "Email already exists! Please chosse a different one"
+                )
+
+def choice_query():
+    return Course.query
+
+class NewLessonForm(FlaskForm):
+    course = QuerySelectField("Course", query_factory=choice_query, get_label="title")
+    title = StringField("Lesson Title", validators=[DataRequired(), Length(max=100)])
+    slug = StringField(
+        "Slug",
+        validators=[DataRequired(), Length(max=32)],
+        render_kw={
+            "placeholder": "Descriptive short version of your title. SEO friendly"
+        },
+    )
+    content = CKEditorField(
+        "Lesson Content", validators=[DataRequired()], render_kw={"rows": "20"}
+    )
+    thumbnail = FileField(
+        "Thumbnail", validators=[DataRequired(), FileAllowed(["jpg", "png"])]
+    )
+    submit = SubmitField("Post")
+
+
+class NewCourseForm(FlaskForm):
+    title = StringField("Course Name", validators=[DataRequired(), Length(max=50)])
+    description = TextAreaField(
+        "Course Description", validators=[DataRequired(), Length(max=150)]
+    )
+    icon = FileField("Icon", validators=[DataRequired(), FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Create")
+
+    def validate_title(self, title):
+        course = Course.query.filter_by(title=title.data).first()
+        if course:
+            raise ValidationError(
+                "Course name already exists! Please choose a different one"
+            )
+        
+class LessonUpdateForm(NewLessonForm):
+    thumbnail = FileField("Thumbnail", validators=[FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Update")
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Regexp(
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,32}$"
+            ),
+        ],
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Reset Password")
